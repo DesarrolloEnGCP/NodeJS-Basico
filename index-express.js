5e5b979a690ae4359b0163595fb133c1ccc3d4c7
DesarrolloEnGCP <53362593+DesarrolloEnGCP@users.noreply.github.com>
2019-07-28 13:12:00 -0400

    Update index-express.js

--- index-express.js
+++ index-express.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = 8080
 
-app.get('/', (req, res) => res.send(Hola Mundo ExpressJS!'))
+app.get('/', (req, res) => res.send('Hola Mundo ExpressJS!'))
 
 app.listen(port, () => console.log(`Servidor Web escuchando puerto ${port}!`))
